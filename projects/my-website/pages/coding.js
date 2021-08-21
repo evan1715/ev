6c389d51a7a8ea869a2eedba9ac5b4262c4fdf16
Evan <evan1715@users.noreply.github.com>
2021-08-21 17:17:38 -0400

    created style file for coding page & adding placeholder sections on page

--- projects/my-website/pages/coding.js
+++ projects/my-website/pages/coding.js
@@ -1,10 +1,27 @@
-import styles from '../styles/evan.module.scss';
+import styles from '../styles/coding.module.scss';
 
 const Coding = () => {
 
     return (
         <main className={ styles.main }>
-            Evan's coding page
+            <h1>Evan's coding page</h1>
+            
+            {/* Repository section */}
+            <section className={ styles.section }>
+                <h2 className={ styles.section_header }>Repository</h2>
+                <div className="">
+
+                </div>
+            </section>
+
+            {/* Technologies section */}
+            <section className={ styles.section }>
+                <h2 className={ styles.section_header }>Technologies</h2>
+                <div>
+
+                </div>
+            </section>
+
         </main>
     )
 }
