6039024e7140d829a36c59eca8b600b3dd07c585
Evan <evan1715@users.noreply.github.com>
2021-08-22 16:02:37 -0400

    Added images to coding page.

--- projects/my-website/pages/coding.js
+++ projects/my-website/pages/coding.js
@@ -1,6 +1,20 @@
+import Image from 'next/image';
 import styles from '../styles/coding.module.scss';
+import css from'../images/css3.svg';
+import express from '../images/express.svg';
+import html from '../images/html5.svg';
+import javascript from '../images/javascript.svg';
+import mongodb from '../images/mongodb.svg';
+import mongoose from '../images/mongoosejs.png';
+import nextjs from '../images/nextjs.svg';
+import nodejs from '../images/nodejs.svg';
+import react from '../images/react.svg';
+import redux from '../images/redux.svg';
+import sass from '../images/sass.svg';
 
 const Coding = () => {
+    const images = ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'NodeJS', 'Express', 'NextJS', 'Sass', 'MongoDB', 'Mongoose'];
+    const files = [html, css, javascript, react, redux, nodejs, express, nextjs, sass, mongodb, mongoose];
 
     return (
         <main className={ styles.main }>
@@ -17,11 +31,15 @@ const Coding = () => {
             {/* Technologies section */}
             <section className={ styles.section }>
                 <h2 className={ styles.section_header }>Technologies</h2>
-                <div>
-
+                <div className={ styles.techs }>
+                    { images.map((tech, index) => 
+                        <div className={ styles.techs_tech } key={ tech }>
+                            <h3>{ tech }</h3>
+                            <Image alt={ `${tech} icon` } src={ files[index] } height={ 128 } width={ 128 } />
+                        </div>
+                    )}
                 </div>
             </section>
-
         </main>
     )
 }
