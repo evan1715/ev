54031758cd8144c75fbf820fc5a52b5d2a18f9c3
Evan <evan1715@users.noreply.github.com>
2021-08-23 16:47:06 -0400

    Attempting to learn and integrate Bootstrap.

--- projects/my-website/components/Nav.js
+++ projects/my-website/components/Nav.js
@@ -1,25 +1,40 @@
 import Link from 'next/link.js';
-import styles from '../styles/nav.module.scss';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Container from 'react-bootstrap/Container';
+// import styles from '../styles/nav.module.scss';
 
 const Nav = () => {
     return (
-        <nav className={ styles.nav }>
-            <ul className={ styles.list }>
-                <li className={ styles.li }>
-                    <Link className={ styles.link } href="/">Evan</Link>
-                </li>
-                <li className={ styles.li }>
-                    <Link className={ styles.link } href="/coding">Coding</Link>
-                </li>
-                <li className={ styles.li }>
-                    <Link className={ styles.link } href="/about">About</Link>
-                </li>
-                <li className={ styles.li }>
-                    <Link className={ styles.link } href="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        //With <nav>, we do not need <ul> <li></li> </ul> to list since <nav> is defined
+        <div className="container-fluid">
+            <nav className="navbar bg-secondary bg-gradient">
+                {/* <Container fluid> */}
+                <Link className="nav-link active" aria-current="page" href="/">Evan</Link>
+
+                <Link className="nav-link" href="/coding">Coding</Link>
+
+                <Link className="nav-link" href="/about">About</Link>
+
+                <Link className="nav-link" href="/contact">Contact</Link>
+                {/* </Container> */}
+            </nav>
+        </div>
     );
 }
 
 export { Nav as default }
+
+            // <ul className={ styles.list }>
+            //     <li className={ styles.li }>
+            //         <Link className={ styles.link } href="/">Evan</Link>
+            //     </li>
+            //     <li className={ styles.li }>
+            //         <Link className={ styles.link } href="/coding">Coding</Link>
+            //     </li>
+            //     <li className={ styles.li }>
+            //         <Link className={ styles.link } href="/about">About</Link>
+            //     </li>
+            //     <li className={ styles.li }>
+            //         <Link className={ styles.link } href="/contact">Contact</Link>
+            //     </li>
+            // </ul>
--- projects/my-website/next.config.js
+++ projects/my-website/next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
     reactStrictMode: true,
+    // webpack: (config, options) => {
+    //     config.module.rules.push({
+    //         test: /\.css$/,
+    //         use: ['postcss-loader']
+    //     });
+    //     config.plugins.push([
+    //         require('autoprefixer')
+    //     ])
+    //     return config;
+    // }
+    
 }
