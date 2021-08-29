63cac6feead7c093e9153bdbbced89b6c0d7c495
Evan <evan1715@users.noreply.github.com>
2021-08-29 17:33:32 -0400

    Created blog and project placeholder page files

---
+++ projects/my-website/pages/blog.js
@@ -0,0 +1,12 @@
+import styles from '../styles/evan.module.scss';
+
+const Blog = () => {
+
+    return (
+        <main className={ styles.main }>
+            Evan's blog page
+        </main>
+    )
+}
+
+export { Blog as default }
---
+++ projects/my-website/pages/projects.js
@@ -0,0 +1,16 @@
+const Projects = () => {
+
+    return (
+        <main className="d-flex flex-column">
+            <p>Table/menu showing links to the projects on this website with # tag to move down the page.</p>
+
+            <ul>
+                <li>JK2</li>
+                <li>My Website</li>
+                <li>Recipe</li>
+            </ul>
+        </main>
+    )
+}
+
+export { Projects as default }
