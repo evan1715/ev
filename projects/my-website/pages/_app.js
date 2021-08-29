298f5bb3c59cddc75ad90ac04341764a9dd17f6e
Evan <evan1715@users.noreply.github.com>
2021-08-29 17:56:09 -0400

    updated _app.js

--- projects/my-website/pages/_app.js
+++ projects/my-website/pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head.js';
 import '../styles/globals.scss';
-import Nav from '../components/Nav.js';
+import Navigation from '../components/Navigation.js';
 import Footer from '../components/Footer.js';
 
 function MyApp({ Component, pageProps }) {
@@ -9,8 +9,15 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Evan's Website</title>
                 <meta name="description" content="Evan's website" />
+                {/* Bootstrap */}
+                {/* <link 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
+                    rel="stylesheet" 
+                    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
+                    crossOrigin="anonymous" /> */}
             </Head>
-            <Nav />
+
+            <Navigation />
             <Component {...pageProps} />
             <Footer />
         </>
