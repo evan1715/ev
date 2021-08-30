a50e848ce2d430f2b31d94e6176ba83b66d71b45
Evan <evan1715@users.noreply.github.com>
2021-08-29 20:15:12 -0400

    Created space around main area so footer and nav aren't overlapping.

--- projects/my-website/pages/_app.js
+++ projects/my-website/pages/_app.js
@@ -9,16 +9,12 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Evan's Website</title>
                 <meta name="description" content="Evan's website" />
-                {/* Bootstrap */}
-                {/* <link 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
-                    rel="stylesheet" 
-                    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
-                    crossOrigin="anonymous" /> */}
             </Head>
 
             <Navigation />
-            <Component {...pageProps} />
+            <div className="my-4 py-5">
+                <Component {...pageProps} />
+            </div>
             <Footer />
         </>
     );
