2b6c5f64d6b01bd7bc24c9f83791037201460385
Evan <evan1715@users.noreply.github.com>
2021-08-30 19:30:51 -0400

    Footer now has closer color to navbar, switched Link to anchor for external links, and now target in new tab when clicking.

--- projects/my-website/components/Footer.js
+++ projects/my-website/components/Footer.js
@@ -5,27 +5,25 @@ import external from '../images/external.svg';
 
 const Footer = () => {
     return (
-        <footer className="bg-dark bg-gradient fixed-bottom py-1">
+        <footer className="navbar navbar-dark bg-dark bg-gradient fixed-bottom py-1">
             <Container>
-                
                 <div className="d-flex justify-content-center">
                     {/* fs-4 is for the font size */}
                     <span className="fs-4">Evan McHugh</span>
                 </div>
 
                 <div className="d-flex flex-row justify-content-center">
-                    <Link href="https://github.com/evan1715">
-                        <a className="nav-link">
+                    <a href="https://github.com/evan1715" target="_blank">
+                        <span className="nav-link">
                             GitHub <Image src={ external } />
-                        </a>
-                    </Link>
-                    <Link href="https://www.linkedin.com/in/evan-mchugh-8013a6142/">
-                        <a className="nav-link">
+                        </span>
+                    </a>
+                    <a href="https://www.linkedin.com/in/evan-mchugh-8013a6142/" target="_blank">
+                        <span className="nav-link">
                             LinkedIn <Image src={ external } />
-                        </a>
-                    </Link>
+                        </span>
+                    </a>
                 </div>
-
             </Container>
         </footer>
     );
