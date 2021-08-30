d812609cdeeb3c2bd56ad56c72b58303da8ee7b5
Evan <evan1715@users.noreply.github.com>
2021-08-30 19:31:28 -0400

    Home page now has placeholder sized sections.

--- projects/my-website/pages/index.js
+++ projects/my-website/pages/index.js
@@ -2,8 +2,20 @@
 //Can't import global CSS file. That's done in _app.js
 
 const Home = () => (
-    <main className="d-flex justify-content-center">
-        Evan's home page
+    <main className="d-flex justify-content-center flex-column">
+        <section className="d-flex align-items-center justify-content-center border border-success vh-100">
+            <h1 className="h-50">Intro section</h1>
+        </section>
+
+        <section className="d-flex align-items-center justify-content-center border border-info vh-100">
+            Evan's home page
+        </section>
+
+        <section className="d-flex align-items-center justify-content-center border border-success vh-100">
+            Major projects
+        </section>
+
+
     </main>
 );
 
