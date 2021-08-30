fcf2b5c49d22ecd5da1ec7e3ed31d3447267bae4
Evan <evan1715@users.noreply.github.com>
2021-08-29 20:18:25 -0400

    Moved some links to footer. Switched to tab stlyed nav. Got rid of included files. Put dividers on the outside. Added div to center main links.

--- projects/my-website/components/Navigation.js
+++ projects/my-website/components/Navigation.js
@@ -1,14 +1,13 @@
-import Image from 'next/image.js';
 import Link from 'next/link.js';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import external from '../images/external.svg';
 
 const Navigation = () => {
     //With <nav>, we do not need <ul> <li></li> </ul> to list since <nav> is defined
     //expand="sm" md lg xl xxl will configure when it will be collapsed or expanded on window size
+    //Link doesn't accept a class/className prop. So, I'll use <a> tags to pass styles.
     return (
         <header className="fixed-top">
             <Navbar bg="navbar navbar-dark bg-secondary bg-gradient" collapseOnSelect expand="lg">
@@ -17,33 +16,22 @@ const Navigation = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        {/* Link doesn't accept a class/className prop. So, I'll use <a> tags to pass styles. */}
-                        <Nav className="me-auto">
-                            <NavDropdown.Divider />
+
+                        <div className="me-auto" />
+
+                        <NavDropdown.Divider />
+                        <Nav variant="tabs" className="me-auto d-flex">
                             <Link href="/coding"><a className="nav-link">Coding</a></Link>
                             <Link href="/projects"><a className="nav-link">Projects</a></Link>
                             <Link href="/blog"><a className="nav-link">Blog</a></Link>
                         </Nav>
 
-                        <Nav className="me-auto">
-                            <NavDropdown.Divider />
-                                <Link href="https://github.com/evan1715">
-                                    <a className="nav-link">
-                                        GitHub <Image src={ external } />
-                                    </a>
-                                </Link>
-                                <Link href="https://www.linkedin.com/in/evan-mchugh-8013a6142/">
-                                    <a className="nav-link">
-                                        LinkedIn <Image src={ external } />
-                                    </a>
-                                </Link>
-                        </Nav>
-
-                        <Nav>
-                            <NavDropdown.Divider />
+                        <NavDropdown.Divider />
+                        <Nav variant="tabs">
                             <Link href="/contact"><a className="nav-link">Contact</a></Link>
                             <Link href="/about"><a className="nav-link">About</a></Link>
                         </Nav>
+                        
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
