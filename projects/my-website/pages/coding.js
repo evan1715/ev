e9f94682e61b122d99470af277ee08557280e670
Evan <evan1715@users.noreply.github.com>
2021-09-04 14:52:11 -0400

    Added cert images to coding page.

--- projects/my-website/pages/coding.js
+++ projects/my-website/pages/coding.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Container from 'react-bootstrap/Container';
 import bootstrap from '../images/bootstrap.svg';
+//Technology logos
 import css from'../images/css3.svg';
 import express from '../images/express.svg';
 import html from '../images/html5.svg';
@@ -12,6 +13,11 @@ import nodejs from '../images/nodejs.svg';
 import react from '../images/react.svg';
 import redux from '../images/redux.svg';
 import sass from '../images/sass.svg';
+//Certificate images
+import fcc_html_css from '../images/fcc_html_css_certificate.png';
+import fcc_js from '../images/fcc_js_certificate.png';
+import udemy_nodejs from '../images/udemy_nodejs_certificate.jpg';
+import udemy_reactjs from '../images/udemy_reactjs_certificate.jpg'
 
 const Coding = () => {
     const images = ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js', 'Express', 'Next.js', 'Sass', 'Bootstrap', 'MongoDB', 'Mongoose'];
@@ -19,11 +25,11 @@ const Coding = () => {
 
     return (
         <Container className="d-flex flex-column align-items-center">
-            <h1>Evan's coding page</h1>
-            
+            <h1 className="p-2">Evan's coding page</h1>
+    
             {/* Repository section */}
             <section className="w-100">
-                <h2 className="pb-4">Repository</h2>
+                <h2 className="p-2">Repository</h2>
                 {/* Table */}
                 <div className="bg-dark bg-gradient" role="grid">
                     {/* Table headers */}
@@ -42,7 +48,7 @@ const Coding = () => {
 
             {/* Technologies section */}
             <section className="w-100">
-                <h2 className="pb-4">Technologies</h2>
+                <h2 className="p-2">Technologies</h2>
                 <div className="bg-dark bg-gradient d-flex flex-wrap justify-content-around">
                     { images.map((tech, index) => 
                         <div className="p-4" key={ tech }>
@@ -50,6 +56,17 @@ const Coding = () => {
                             <Image alt={ `${tech} icon` } src={ files[index] } height={ 128 } width={ 128 } />
                         </div>
                     )}
+                </div>
+            </section>
+
+            {/* Certifications section */}
+            <section className=" vh-100 w-100">
+                <h2 className="p-2">Certifications</h2>
+                <div className="d-flex flex-wrap justify-content-around">
+                    <Image src={ fcc_html_css } height={ 225 } width={ 300 } />
+                    <Image src={ fcc_js } height={ 225 } width={ 300 } />
+                    <Image src={ udemy_nodejs } height={ 225 } width={ 300 } />
+                    <Image src={ udemy_reactjs } height={ 225 } width={ 300 } />
                 </div>
             </section>
         </Container>
