5eb5c1a8fb75688ac59f2cbc79a07147f73e2d01
Evan <evan1715@users.noreply.github.com>
2021-09-04 14:52:38 -0400

    Trying out cards on project page.

--- projects/my-website/pages/projects.js
+++ projects/my-website/pages/projects.js
@@ -1,7 +1,29 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+//Images
+import chat_app from '../images/screenshot_chat_app.png';
+import indecision_app from '../images/screenshot_indecision_app.png';
+//import my_website from '../images_screenshot_my_website';
+import task_manager from '../images/screenshot_task_manager_app.jpg';
+import recipe_project from '../images/screenshot_recipe_project.png';
 
 const Projects = () => (
     <Container className="border border-secondary d-flex flex-column align-items-center">
+        <Row>
+            <Col lg={ true }>
+                <Card border="warning" className="bg-dark bg-gradient">
+                    <Card.Img variant="top" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>Card text</Card.Text>
+                        <Card.Link href="#">Card link 1</Card.Link>
+                        <Card.Link href="#">Card link 2</Card.Link>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
         <p>Table/menu showing links to the projects on this website with # tag to move down the page.</p>
 
         <ul>
