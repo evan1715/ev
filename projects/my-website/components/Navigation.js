ed4832e68e54f546700ddc56a0a9641827ae534d
Evan <evan1715@users.noreply.github.com>
2021-09-06 15:56:09 -0400

    Added useEffect and useState to display active links in navigation if page was loaded by other means of click event on nav.

--- projects/my-website/components/Navigation.js
+++ projects/my-website/components/Navigation.js
@@ -1,15 +1,21 @@
 import { useRouter } from 'next/router.js';
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Navigation = () => {
     const router = useRouter();
+    const [isActive, setIsActive] = useState();
 
     const handleRoute = (e, route) => {
         e.preventDefault();
         router.push(route);
     }
+
+    useEffect(() => {
+        setIsActive(router.pathname);
+    }, [router.pathname]);
 
     return (
         <Navbar collapseOnSelect bg="navbar navbar-dark bg-dark bg-gradient" expand="lg" sticky="top" variant="dark">
@@ -23,20 +29,23 @@ const Navigation = () => {
 
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="justify-content-sm-evenly w-100" variant="tabs">
-                        <Nav.Link eventKey="coding" onClick={ (e) => handleRoute(e, '/coding') }>
+                        <Nav.Link active={ isActive === '/coding' && true } eventKey="coding" onClick={ (e) => handleRoute(e, '/coding') }>
                             <div /><div />Coding
                         </Nav.Link>
-                        <Nav.Link eventKey="projects" onClick={ (e) => handleRoute(e, '/projects') }>
+
+                        <Nav.Link active={ isActive === '/projects' && true } eventKey="projects" onClick={ (e) => handleRoute(e, '/projects') }>
                             <div /><div />Projects
                         </Nav.Link>
-                        <Nav.Link eventKey="blog" onClick={ (e) => handleRoute(e, '/blog') }>
+
+                        <Nav.Link active={ isActive === '/blog' && true } eventKey="blog" onClick={ (e) => handleRoute(e, '/blog') }>
                             <div /><div />Blog
                         </Nav.Link>
 
-                        <Nav.Link eventKey="contact" onClick={ (e) => handleRoute(e, '/contact') }>
+                        <Nav.Link active={ isActive === '/contact' && true } eventKey="contact" onClick={ (e) => handleRoute(e, '/contact') }>
                             <div /><div />Contact
                         </Nav.Link>
-                        <Nav.Link eventKey="about" onClick={ (e) => handleRoute(e, '/about') }>
+
+                        <Nav.Link active={ isActive === '/about' && true } eventKey="about" onClick={ (e) => handleRoute(e, '/about') }>
                             <div /><div />About
                         </Nav.Link>
                     </Nav>
