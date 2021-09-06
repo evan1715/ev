bf371270d8b046e06ce55ed39c2a81bc53828fda
Evan <evan1715@users.noreply.github.com>
2021-09-05 20:33:17 -0400

    Changed up home page to add cards. Changed styling too. Added animated dividers/borders.

--- projects/my-website/pages/index.js
+++ projects/my-website/pages/index.js
@@ -1,22 +1,23 @@
-//This is the homepage.
-//Can't import global CSS file. That's done in _app.js
+import ProjectCards from '../components/ProjectCards.js';
 
 const Home = () => (
     <>
-        <section className="d-flex align-items-center justify-content-center border border-success vh-100">
-            <h1 className="h-50 mainTitle">Evan McHugh</h1>
+        <section className="border-start border-end border-success m-2 d-flex align-items-center justify-content-center vh-100">
+            <h1 className="h-50 main-title">Evan McHugh</h1>
             <p>Self-learning software developer.</p>
         </section>
+        
+        <div className="card-animation"><i /></div>
 
-        <section className="d-flex align-items-center justify-content-center border border-info vh-100">
+        <section className="bg-dark bg-opacity-50 border-orange m-2 d-flex align-items-center justify-content-center min-vh-100">
+            <ProjectCards />
+        </section>
+
+        <div className="card-animation"><i /></div>
+
+        <section className="border-start border-end border-info m-2 d-flex align-items-center justify-content-center vh-100">
             Evan's home page
         </section>
-
-        <section className="d-flex align-items-center justify-content-center border border-success vh-100">
-            Major projects
-        </section>
-
-
     </>
 );
 
