c0caad381334f6b9e970776d6353da6d4d759439
Evan <evan1715@users.noreply.github.com>
2021-09-07 14:18:34 -0400

    Added comments to the index file.

--- projects/my-website/pages/index.js
+++ projects/my-website/pages/index.js
@@ -2,6 +2,7 @@ import ProjectCards from '../components/ProjectCards.js';
 
 const Home = () => (
     <>
+        {/* Intro section */}
         <section className="border-start border-end border-success m-2 d-flex align-items-center justify-content-center vh-100">
             <h1 className="h-50 main-title">Evan McHugh</h1>
             <p>Self-learning software developer.</p>
@@ -10,6 +11,7 @@ const Home = () => (
         {/* Animated border between sections */}
         <div className="card-animation"><i /></div>
 
+        {/* Middle section: Project cards */}
         <section className="bg-dark bg-opacity-50 border-orange m-2 d-flex align-items-center justify-content-center min-vh-100">
             <ProjectCards />
         </section>
@@ -17,6 +19,7 @@ const Home = () => (
         {/* Animated border between sections */}
         <div className="card-animation"><i /></div>
 
+        {/* Last section  */}
         <section className="border-start border-end border-info m-2 d-flex align-items-center justify-content-center vh-100">
             Evan's home page
         </section>
