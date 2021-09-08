bf97d3cb60c10ee9a7e56bffdeb7c0b2ccb97466
Evan <evan1715@users.noreply.github.com>
2021-09-07 20:01:31 -0400

    Accordion now adjusts which will be open/active based on URL. If none designated, it'll default to Recipe Project.

--- projects/my-website/components/ProjectAccordion.js
+++ projects/my-website/components/ProjectAccordion.js
@@ -1,16 +1,30 @@
 // import Image from 'next/image';
 // import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Accordion from 'react-bootstrap/Accordion';
 import pjInfo from '../data/projects-info.json';
 
 const ProjectAccordion = () => {
+    const router = useRouter();
+    const [selectedProject, setSelectedProject] = useState(5);
+
+    useEffect(() => {
+        const id = router.asPath.split('#')[1];
+        pjInfo.map((pj, index) => {
+            if (pj.id == id) {
+                setSelectedProject(index);
+            }
+        })
+    }, [router.asPath]);
+
     return (
         <Container className="p-0">
             <h2 className="p-2">Projects</h2>
-            <Accordion defaultActiveKey="0" flush>
+            <Accordion activeKey={ `${selectedProject}` } flush>
                 { pjInfo.map((project, index) => 
-                    <Accordion.Item className="bg-light-gray" eventKey={ index } key={ index }>
+                    <Accordion.Item className="bg-light-gray" eventKey={ `${index}` } id={ project.id } key={ index } onClick={ () => console.log(index)} >
                         <Accordion.Header className="bg-light-gray">
                             { project.title } <div className="ps-2 text-muted">- { project.subtitle }</div>
                             </Accordion.Header>
--- projects/my-website/pages/_app.js
+++ projects/my-website/pages/_app.js
@@ -1,8 +1,8 @@
 import Head from 'next/head.js';
+import Container from 'react-bootstrap/Container';
 import '../styles/globals.scss';
 import Navigation from '../components/Navigation.js';
 import Footer from '../components/Footer.js';
-import Container from 'react-bootstrap/Container';
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -22,6 +22,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export { MyApp as default }
-
-//Can't import global CSS file into index.js. That's done in _app.js
-//CSS files have to have the file name *.module.css
