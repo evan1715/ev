876f25fedf83ed4c54c7e871cafc8e1ca03110ef
Evan <evan1715@users.noreply.github.com>
2021-09-27 18:27:52 -0400

    Redid a bit of the homepage. Added on things and styling. Also adjusted for better mobile screens.

--- projects/my-website/src/pages/index.js
+++ projects/my-website/src/pages/index.js
@@ -1,44 +1,71 @@
+import { useRouter } from 'next/router';
+import Container from 'react-bootstrap/Container';
 import ProjectCards from '../components/ProjectCards.js';
 import Technologies from '../components/Technologies.js';
 
-const Home = () => (
-    <>
-        {/* Intro section */}
-        <section className="border-start border-end border-success m-2 d-flex flex-column align-items-center justify-content-center vh-100">
-            <>
-                <h1 className="h-50 main-title">Evan McHugh</h1>
-                <h2>~~ WEBSITE IS INCOMPLETE -- Still working on it. ~~</h2>
-                <p>Self-learning software developer.</p>
-            </>
-            <>
-                <Technologies mobile={ true } />
-            </>
-        </section>
 
-        {/* Animated border between sections */}
-        <div className="card-animation"><i /></div>
+const Home = () => {
+    const router = useRouter();
 
-        {/* Middle section: Project cards */}
-        <section className="bg-dark bg-opacity-50 border-orange m-2 d-flex align-items-center justify-content-center min-vh-100">
-            <ProjectCards />
-        </section>
+    return (
+        <>
+            {/* Intro section */}
+            <section className="border-start border-end border-success m-2 d-flex flex-column align-items-center justify-content-center vh-100">
 
-        {/* Animated border between sections */}
-        <div className="card-animation"><i /></div>
+                <h1 className="main-title h-25">Evan McHugh</h1>
 
-        {/* Last section  */}
-        <section className="m-2 bottom-section p-2">
-            <div>Next adventures? As of 9-20-2021
-                <ul>
-                    <li>To finish this website and continue learning NextJS and Bootstrap, as well as expanding my current knowledge.</li>
-                    <li>Learn MySQL and PHP</li>
-                    <li>Learn the proper ways to make a website fully accessible.</li>
-                </ul>
-                Note: You may have noticed the displaced mustache on the side-scrolling technology images. 
-                I left the moustache like this on purpose because I found it humorous to have a displaced mustache.
-            </div>
-        </section>
-    </>
-);
+                <div className="d-flex flex-column align-items-center h-25 px-4">
+                    <p className="h4">Self-learning software developer through research and development with a high interest in technology.</p>
+                    <div className="h4 link-info mt-1" onClick={ () => router.push('/coding#recipe-project') }>
+                        Check out my highlighted work!
+                    </div>
+                </div>
+
+                <div>
+                    <Technologies mobile={ true } />
+                </div>
+            </section>
+
+            {/* Animated border between sections */}
+            <div className="card-animation"><i /></div>
+
+            {/* Middle section: Project cards */}
+            <section className="bg-dark bg-opacity-50 border-orange m-2 d-flex align-items-center justify-content-center min-vh-100">
+                <ProjectCards />
+            </section>
+
+            {/* Animated border between sections */}
+            <div className="card-animation"><i /></div>
+
+            {/* Last section  */}
+            <section className="m-2 bottom-section p-2">
+                <Container className="d-flex flex-column py-5">
+                    <div className="m-1 p-1">
+                        <h6>Next adventures? As of 9-20-2021</h6>
+                        <ul>
+                            <li>To finish this website and continue learning NextJS and Bootstrap, as well as expanding my current knowledge.</li>
+                            <li>Learn MySQL and PHP</li>
+                            <li>Learn the proper ways to make a website fully accessible.</li>
+                        </ul>
+                    </div>
+
+                    <div className="m-1 p-1 my-5 py-5 border-top border-bottom">
+                        <h6>Need more information?</h6>
+                        <ul>
+                            <li>View my contact page to get in contact with me!</li>
+                            <li>Look at my coding page to get a view of project and more!</li>
+                        </ul>
+                    </div>
+
+                    <div className="m-1 p-1">
+                        <h6>Side note</h6>
+                        <p className="m-0">You may have noticed the displaced mustache on the side-scrolling technology images.</p>
+                        <p className="m-0">I left the moustache like this on purpose because I found it humorous to have a displaced mustache.</p>
+                    </div>
+                </Container>
+            </section>
+        </>
+    );
+}
 
 export { Home as default }
