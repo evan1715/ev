2a2139ae02c64aa2a58c35e070ca70d9d628a65a
Evan <evan1715@users.noreply.github.com>
2021-09-27 18:34:33 -0400

    Added msg on About page.

--- projects/my-website/src/pages/about.js
+++ projects/my-website/src/pages/about.js
@@ -2,14 +2,20 @@ import Container from 'react-bootstrap/Container';
 
 const About = () => (
     <Container className="border border-warning p-4 d-flex justify-content-center flex-column">
-        <div className="w-100 bar-color1">cyan</div>
-        <div className="w-100 bar-color2">turquoise</div>
-        <div className="w-100 bar-color3">teal</div>
-        <div className="w-100 bar-color4">light green</div>
-        <div className="w-100 bar-color5">green</div>
-        <div className="w-100 bar-color6">dark green</div>
-        <div className="w-100 bar-color7">orange</div>
-        <div className="w-100 bar-color8">dark orange/brown</div>
+        <h1 className="text-center">About</h1>
+        <p className="text-center">This page hasn't been completed yet. I apologize and it will be ready soon. Meanwhile, enjoy the colors! ;p</p>
+        
+        <div className="mt-5">
+            <h4 className="text-center">Color theme of website compared</h4>
+            <div className="w-100 bar-color1">cyan</div>
+            <div className="w-100 bar-color2">turquoise</div>
+            <div className="w-100 bar-color3">teal</div>
+            <div className="w-100 bar-color4">light green</div>
+            <div className="w-100 bar-color5">green</div>
+            <div className="w-100 bar-color6">dark green</div>
+            <div className="w-100 bar-color7">orange</div>
+            <div className="w-100 bar-color8">dark orange/brown</div>
+        </div>
     </Container>
 );
 
