2fe2393ee9d39811f3e6557cd7fdbf4bc33fc3ac
Evan McHugh <evan1715@hotmail.com>
2023-09-11 17:11:12 -0400

    added jsdoc to callfetch

--- packages/passage/utils/callFetch.js
+++ packages/passage/utils/callFetch.js
@@ -5,7 +5,26 @@ const nodeFruit = require('../fruit');
 //If there's a body, convert it. If it throws an error, then it didn't have a body.
 const convertBody = (res) => res.json().catch(() => res);
 
-//This is where the actual fetch call is made.
+/**
+ * This is where the actual fetch call is made.
+ * @param {object} arg - The deconstructed object.
+ *
+ * @param {string} arg.path - The path to the desired API call like /user/login.
+ *
+ * @param {object} arg.options - The options for the fetch call.
+ * @param {object} arg.options.headers - The headers for the fetch call.
+ * @param {string} arg.options.method - GET, POST, PATCH, DELETE.
+ * @param {object} [arg.options.body] - The body of the request (optional).
+ *
+ * @param {object} arg.config - The config object.
+ * @param {string} arg.config.baseURL - The base URL for the API.
+ * @param {number} [arg.config.timer=10000] - The timer for the fetch call. Default is 10000 (optional).
+ * @param {string} arg.config.environment - The environment for the fetch call.
+ * @param {boolean} [arg.config.rethrow=false] - Whether to rethrow the error. Default is false (optional).
+ *
+ * @param {number} [arg.retries=0] - The number of times to retry the call (optional).
+ * @returns {Promise<(object|string)>} - The response from the API call.
+ */
 const callFetch = ({ path, options, retries = 0, config }) => {
     const controller = new AbortController();
     const fruit = config.environment === 'browser' ? browserFruit : nodeFruit;
